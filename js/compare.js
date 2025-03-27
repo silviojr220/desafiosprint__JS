0643ea03fe340e4fe606743f4a4b5aa8f238543c
SilvioClaudioJr <silvioclaudiojr10@gmail.com>
2025-03-27 16:52:29 -0300

    alterações

--- js/compare.js
+++ js/compare.js
@@ -3,46 +3,51 @@
 let carArr = [];
 
 class Car {
-    constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image){
-         this.nome = nome,
-         this.preco = preco,
-         this.alturaCacamba = alturaCacamba,
-         this.alturaVeiculo = alturaVeiculo,
-         this.alturaSolo = alturaSolo,
-         this.capacidadeCarga = capacidadeCarga,
-         this.motor = motor,
-         this.potencia = potencia,
-         this.volumeCacamba = volumeCacamba,
-         this.roda = roda,
-         this.image = image
+    constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image) {
+        this.nome = nome,
+            this.preco = preco,
+            this.alturaCacamba = alturaCacamba,
+            this.alturaVeiculo = alturaVeiculo,
+            this.alturaSolo = alturaSolo,
+            this.capacidadeCarga = capacidadeCarga,
+            this.motor = motor,
+            this.potencia = potencia,
+            this.volumeCacamba = volumeCacamba,
+            this.roda = roda,
+            this.image = image
     }
-} 
+}
 
 // search on array if exist carClass returning 1 if not return -1
 function GetCarArrPosition(arr, carClass) {
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i].nome  === carClass.nome)
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].nome === carClass.nome)
             return i;
     }
     return -1;
 }
 
 function SetCarToCompare(el, carClass) {
-   
-    if(carClass instanceof Car){       
-        if(el.checked){
-                carArr.push(carClass);
-            
-        } else {
-          
-        } 
+    if (!(carClass instanceof Car)) {
+        throw new Error("Você precisa passar um objeto da classe Car.");
+    }
+    if (el.checked) {
+        if (carArr.length >= 2) {
+            alert('Você só pode comparar dois carros!');
+            el.checked = false;
+            return;
+        }
+        carArr.push(carClass);
     } else {
-        throw "You need set a Car Class";
+        let i = GetCarArrPosition(carArr, carClass)
+        if (i !== -1) {
+            carArr.splice(i, 1);
+        }
     }
 }
 
 function ShowCompare() {
-    if(carArr.length != 2) {
+    if (carArr.length != 2) {
         alert("Precisa marcar 2 carros para apresentar a comparação");
         return;
     }
@@ -51,23 +56,22 @@ function ShowCompare() {
     document.getElementById("compare").style.display = "block";
 }
 
-function HideCompare(){
-    document.getElementById("compare").style.display = "none"; 
+function HideCompare() {
+    document.getElementById("compare").style.display = "none";
 }
 
 function UpdateCompareTable() {
-    
-carArr.slice(0, 2).map((car, i) => {
-    document.getElementById(`compare_image_${i}`).innerHTML = (`<img src="${car.image}"width='200'>`);
-    document.getElementById(`compare_modelo_${i}`).textContent = car.nome;
-    document.getElementById(`compare_alturacacamba_${i}`).textContent = car.alturaCacamba;
-    document.getElementById(`compare_alturaveiculo_${i}`).textContent = car.alturaVeiculo;
-    document.getElementById(`compare_alturasolo_${i}`).textContent = car.alturaSolo;
-    document.getElementById(`compare_capacidadecarga_${i}`).textContent = car.capacidadeCarga;
-    document.getElementById(`compare_motor_${i}`).textContent = car.motor;
-    document.getElementById(`compare_potencia_${i}`).textContent = car.potencia;
-    document.getElementById(`compare_volumecacamba_${i}`).textContent = car.volumeCacamba;
-    document.getElementById(`compare_roda_${i}`).textContent = car.roda;
-    document.getElementById(`compare_preco_${i}`).textContent = `${car.preco.toLocaleString('pt-br', {style:'currency', currency:'BRL'})}`;
-});
+    carArr.slice(0, 2).map((car, i) => {
+        document.getElementById(`compare_image_${i}`).innerHTML = (`<img src="${car.image}"width='200'>`);
+        document.getElementById(`compare_modelo_${i}`).textContent = car.nome;
+        document.getElementById(`compare_alturacacamba_${i}`).textContent = car.alturaCacamba;
+        document.getElementById(`compare_alturaveiculo_${i}`).textContent = car.alturaVeiculo;
+        document.getElementById(`compare_alturasolo_${i}`).textContent = car.alturaSolo;
+        document.getElementById(`compare_capacidadecarga_${i}`).textContent = car.capacidadeCarga;
+        document.getElementById(`compare_motor_${i}`).textContent = car.motor;
+        document.getElementById(`compare_potencia_${i}`).textContent = car.potencia;
+        document.getElementById(`compare_volumecacamba_${i}`).textContent = car.volumeCacamba;
+        document.getElementById(`compare_roda_${i}`).textContent = car.roda;
+        document.getElementById(`compare_preco_${i}`).textContent = `${car.preco.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}`;
+    });
 }
--- js/form.js
+++ js/form.js
@@ -1,11 +1,10 @@
 
 //class contato
 class contato {
-    constructor(nome, sobrenome, email, cpf, telefone, contato) {
+    constructor(nome, sobrenome, email, telefone, contato) {
         this.nome = nome;
         this.sobrenome = sobrenome
         this.email = email;
-        this.cpf = cpf;
         this.telefone = telefone;
         this.contato = contato;
     }
@@ -16,7 +15,6 @@ function Post(form) {
     let data = new contato(form.elements.namedItem("nome").value,
         form.elements.namedItem("sobrenome").value,
         form.elements.namedItem("email").value,
-        form.elements.namedItem("cpf").value,
         form.elements.namedItem("telefone").value,
         form.elements.namedItem("contato").value);
     Enviar(data)
@@ -25,6 +23,8 @@ function Post(form) {
 function Enviar(data) {
     if (data.nome != "") {
         alert('Obrigado sr(a) ' + data.nome + ' os seus dados foram encaminhados com sucesso');
-    console.log(data);}
-     
+        event.preventDefault();
+        console.table(data);
+    }
+
 }
